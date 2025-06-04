66f956eab801d24bf5a982fccceb99e4631d4faf
Mihey <miheyzhukov@yandex.ru>
2025-06-04 23:34:20 +0300

    Исправление ошибок

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/NewBookingRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/NewBookingRequest.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.booking.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
@@ -8,7 +10,9 @@ import java.time.LocalDateTime;
 
 @Data
 public class NewBookingRequest {
+    @FutureOrPresent(message = "Нельзя начать бронирование раньше текущего дня!")
     private LocalDateTime start;
+    @Future(message = "Нельзя закончить бронирование в этот же день или раньше!")
     private LocalDateTime end;
     @NotNull(message = "У вещи должен быть ID")
     @Positive(message = "ID вещи не может быть отрицательным числом")
