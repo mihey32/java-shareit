8577b717e0d5e0afce9469fec0a670ae69cfb21c
Mihey <miheyzhukov@yandex.ru>
2025-06-04 23:26:43 +0300

    checkstyle_1.1

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -16,52 +16,52 @@ import ru.practicum.shareit.enums.States;
 @RequiredArgsConstructor
 @Validated
 public class BookingController {
-	private final BookingClient bookingClient;
+    private final BookingClient bookingClient;
 
-	@GetMapping("/{booking-id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Object> findBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
-											  @PathVariable("booking-id") Long bookingId) {
-		return bookingClient.getBooking(userId, bookingId);
-	}
+    @GetMapping("/{booking-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> findBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                              @PathVariable("booking-id") Long bookingId) {
+        return bookingClient.getBooking(userId, bookingId);
+    }
 
-	@PostMapping
-	public ResponseEntity<Object> bookItem(@RequestHeader("X-Sharer-User-Id") Long userId,
-										   @Valid @RequestBody NewBookingRequest requestDto) {
-		return bookingClient.bookItem(userId, requestDto);
-	}
+    @PostMapping
+    public ResponseEntity<Object> bookItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                           @Valid @RequestBody NewBookingRequest requestDto) {
+        return bookingClient.bookItem(userId, requestDto);
+    }
 
-	@GetMapping
-	public ResponseEntity<Object> findAllBookingsByUser(@RequestHeader("X-Sharer-User-Id") Long userId,
-														@RequestParam(name = "state", defaultValue = "ALL") String stateParam) {
-		States state = States.from(stateParam)
-				.orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
-		return bookingClient.getBookings(null, userId, state);
-	}
+    @GetMapping
+    public ResponseEntity<Object> findAllBookingsByUser(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                        @RequestParam(name = "state", defaultValue = "ALL") String stateParam) {
+        States state = States.from(stateParam)
+                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+        return bookingClient.getBookings(null, userId, state);
+    }
 
-	@GetMapping("/owner")
-	public ResponseEntity<Object> findAllBookingsByOwnerItems(@RequestHeader("X-Sharer-User-Id") Long userId,
-															  @RequestParam(name = "state", defaultValue = "ALL") String stateParam) {
-		States state = States.from(stateParam)
-				.orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
-		return bookingClient.getBookings("/owner", userId, state);
-	}
+    @GetMapping("/owner")
+    public ResponseEntity<Object> findAllBookingsByOwnerItems(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                              @RequestParam(name = "state", defaultValue = "ALL") String stateParam) {
+        States state = States.from(stateParam)
+                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+        return bookingClient.getBookings("/owner", userId, state);
+    }
 
-	@PutMapping
-	public ResponseEntity<Object> updateBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
-												@RequestBody UpdateBookingRequest newBooking) {
-		return bookingClient.updateBooking(userId, newBooking);
-	}
+    @PutMapping
+    public ResponseEntity<Object> updateBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @RequestBody UpdateBookingRequest newBooking) {
+        return bookingClient.updateBooking(userId, newBooking);
+    }
 
-	@DeleteMapping("/{booking-id}")
-	public ResponseEntity<Object> deleteBooking(@PathVariable("booking-id") Long bookingId) {
-		return bookingClient.deleteBooking(bookingId);
-	}
+    @DeleteMapping("/{booking-id}")
+    public ResponseEntity<Object> deleteBooking(@PathVariable("booking-id") Long bookingId) {
+        return bookingClient.deleteBooking(bookingId);
+    }
 
-	@PatchMapping("/{booking-id}")
-	public ResponseEntity<Object> approveBooking(@PathVariable("booking-id") Long bookingId,
-												 @RequestHeader("X-Sharer-User-Id") Long userId,
-												 @RequestParam(name = "approved", defaultValue = "false") Boolean approved) {
-		return bookingClient.approveBooking(bookingId, userId, approved);
-	}
+    @PatchMapping("/{booking-id}")
+    public ResponseEntity<Object> approveBooking(@PathVariable("booking-id") Long bookingId,
+                                                 @RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @RequestParam(name = "approved", defaultValue = "false") Boolean approved) {
+        return bookingClient.approveBooking(bookingId, userId, approved);
+    }
 }
